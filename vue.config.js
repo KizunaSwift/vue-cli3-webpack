7be590d0d6851d4e48db0116739f94d3164ba771
三三斑 <1013541707@qq.com>
2019-08-29 11:38:30 +0800

    Update vue.config.js
    update config

--- vue.config.js
+++ vue.config.js
@@ -9,24 +9,25 @@ module.exports = {
   outputDir: 'dist',
   configureWebpack: {
     resolve: {
-      extensions: ['.css'],
-      alias: {
-        'vue$': 'vue/dist/vue.esm.js',
-        '@': resolve('src')
-      }
+      extensions: ['.css']
     }
   },
   lintOnSave: false,
+  productionSourceMap: false,
   devServer: {
     host: 'localhost',
     port: '8080',
     open: true,
     proxy: {
       '/api': {
+        // target: 'http://sit.xxx.com',
         target: 'http://api.douban.com',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
+        },
+        onProxyReq(proxyReq) { // 访问生产环境
+          proxyReq.setHeader('Cookie', 'authId=sicvgghhh;token=dghhh;')
         }
       }
     }
